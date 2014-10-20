128e90fb509e38e72df70250fc602fabb00ef094
Petro Yakubiv <petro.yakubiv@gmail.com>
2014-10-20 23:48:45 +0300

    first commit

---
+++ app/controllers/demo_page_controller.rb
@@ -0,0 +1,9 @@
+class DemoPageController < ApplicationController
+  def index
+  	respond_to do |format|
+      format.json
+      format.html          # /app/views/home/index.html.erb
+      format.html.phone    # /app/views/home/index.html+phone.erb
+    end
+  end
+end
---
+++ test/helpers/demo_page_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class DemoPageHelperTest < ActionView::TestCase
+end
