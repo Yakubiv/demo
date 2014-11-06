2d977a1464f656d5666f905751e235794543ed84
Petro Yakubiv <petro.yakubiv@gmail.com>
2014-11-06 15:37:46 +0200

    Opisity

--- spec/features/user_views_path_spec.rb
+++ spec/features/user_views_path_spec.rb
@@ -1,10 +1,9 @@
 require "rails_helper"
 
-feature "User click on " do 
-	scenario "views path" do
+feature "User click on" do 
+	scenario "path" do
 		visit root_path
 		click_link "Path"
-
 		expect(page).to have_content("Path")
 
 	end
