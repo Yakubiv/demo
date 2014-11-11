1b6887184cdf3f1a00fb70b016fa37ced891e5ee
Petro Yakubiv <petro.yakubiv@gmail.com>
2014-11-11 12:34:39 +0200

    New style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require ckeditor-jquery
+//= require ckeditor/config
 //= require jquery
 //= require jquery_ujs  
 //= require_tree .
