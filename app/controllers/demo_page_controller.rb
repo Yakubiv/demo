1f4ff510a5f4283e8f59de7e4d7b8e21545a64c9
Petro Yakubiv <petro.yakubiv@gmail.com>
2014-11-14 11:21:36 +0200

    Titles for pages

--- app/controllers/demo_page_controller.rb
+++ app/controllers/demo_page_controller.rb
@@ -1,5 +1,9 @@
 class DemoPageController < ApplicationController
   def index
+  	@recent_posts = Post.order("created_at desc").limit(10).offset(0)
+    if user_signed_in?
+      @recent_user_post = current_user.posts.order("created_at desc").limit(4).offset(0)
+    end
   	@posts = Post.paginate(page: params[:page], per_page: 6)
   	respond_to do |format|
       format.json
